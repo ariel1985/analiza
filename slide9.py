4c4189413b6c15ac857635f3ce3174b598e35c79
ariel1985 <rothemariel@gmail.com>
2024-03-27 21:14:41 +0200

    conditions

---
+++ slide9.py
@@ -0,0 +1,33 @@
+# בצעו השמה למשתנה מסוג INT והדפיסו אותו
+my_int = 16
+print(my_int)
+
+# בצעו השמה לשני משתנים מסוג INT והדפיסו את תוצאת החיבור ביניהם
+num1 = 1
+num2 = 32
+
+print(1 + 32)
+print(num1 + num2)
+
+result = num1 + num2
+print(result)
+
+print(num1, '+', num2, '=',     result)
+# בצעו השמה לשלושה משתנים מסוג INT והדפיסו את תוצאת מכפלתם
+n1 = 2
+n2 = 3
+n3 = 4
+
+print(n1 * n2 * n3)
+
+result = n1 * n2 * n3
+print(result)
+
+print (n1 * 10)
+# בצעו השמה למשתנה מסוג מחרוזת והדפיסו אותה
+str = 'Mishtane'
+print(str)
+
+# בצעו השמה למשתנה מסוג BOOLEAN והדפיסו אותו
+b = False
+print(b)
